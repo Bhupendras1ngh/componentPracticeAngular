9abbc6e0f9e9b8049df2dd5208d37d45f235e151
Bhupendra <bhupendra@gamil.com>
2023-02-04 09:09:49 +0530

    initial commit

---
+++ data.ts
@@ -0,0 +1,1 @@
+console.log("heloo inside the tYpeScript file in the world of Web Devlopment ")
---
+++ table.js
@@ -0,0 +1,2 @@
+let val = [A ,B ,C ,D ,E ,F ,G ,H ,I ,J ,K ,LM ,I ,J ,M];
+val.forEach(i => console.log(i));
